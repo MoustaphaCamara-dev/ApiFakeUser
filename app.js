21b01c2d78e35e469c2bd53c057ac8743cf405ac
Moustapha Camara <99319777+MoustaphaCamara-dev@users.noreply.github.com>
2024-10-10 16:32:12 +0200

    Refactor : Renommage des variables pour plus de clarté

--- app.js
+++ app.js
@@ -1,78 +1,77 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
 const dotenv = require("dotenv");
-const getRandomPassword = require("./passwordGenerator"); // Importer la fonction
+const generateRandomPassword = require("./passwordGenerator");
 
-dotenv.config(); // Charger les variables d'environnement
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const serverPort = 3000;
 
-// Middleware pour analyser le corps des requêtes en JSON
 app.use(express.json());
 
-// Connexion à MongoDB
-const uri = process.env.MONGODB_URI;
-let db;
+const mongoUri = process.env.MONGODB_URI;
+let userDatabase;
 
-MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((client) => {
+MongoClient.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then((mongoClient) => {
     console.log("Connecté à MongoDB");
-    db = client.db("userDB"); // Accéder à la base de données userDB
+    userDatabase = mongoClient.db("userDB");
   })
   .catch(console.error);
 
-// Fonction pour créer un faux utilisateur
-const generateFakeUser = async () => {
-  const [firstName, lastName, domain] = await Promise.all([
-    db
+const createFakeUser = async () => {
+  const [firstNameData, lastNameData, domainData] = await Promise.all([
+    userDatabase
       .collection("firstNames")
       .aggregate([{ $sample: { size: 1 } }])
       .toArray(),
-    db
+    userDatabase
       .collection("lastNames")
       .aggregate([{ $sample: { size: 1 } }])
       .toArray(),
-    db
+    userDatabase
       .collection("domains")
       .aggregate([{ $sample: { size: 1 } }])
       .toArray(),
   ]);
 
   return {
-    firstName: firstName[0].name,
-    lastName: lastName[0].name,
-    email: `${firstName[0].name.toLowerCase()}.${lastName[0].name.toLowerCase()}@${
-      domain[0].domain
+    firstName: firstNameData[0].name,
+    lastName: lastNameData[0].name,
+    email: `${firstNameData[0].name.toLowerCase()}.${lastNameData[0].name.toLowerCase()}@${
+      domainData[0].domain
     }`,
-    password: getRandomPassword(),
+    password: generateRandomPassword(),
   };
 };
 
-// Route pour créer des faux utilisateurs (GET)
 app.get("/api/fake-user", async (req, res) => {
   try {
-    const fakeUser = await generateFakeUser();
-    res.json(fakeUser);
+    const singleFakeUser = await createFakeUser();
+    res.json(singleFakeUser);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-// Route pour créer plusieurs faux utilisateurs (GET)
 app.get("/api/fake-users", async (req, res) => {
   try {
-    const count = Math.max(1, parseInt(req.query.count) || 1); // Nombre d'utilisateurs à générer
-    const fakeUsers = await Promise.all(
-      Array.from({ length: count }, generateFakeUser)
+    const userCount = Math.max(1, parseInt(req.query.count) || 1);
+    const multipleFakeUsers = await Promise.all(
+      Array.from({ length: userCount }, createFakeUser)
     );
-    res.json(fakeUsers);
+    res.json(multipleFakeUsers);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-// Démarrer le serveur
-app.listen(port, () => {
-  console.log(`ApiFakeUser en cours d'exécution sur http://localhost:${port}`);
+app.listen(serverPort, () => {
+  console.log(
+    `ApiFakeUser en cours d'exécution sur http://localhost:${serverPort}`
+  );
 });
